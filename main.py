33525a6210677b8b8a432dd050680b7b47cc2762
Nathan <long3000@gmail.com>
2019-06-19 02:24:59 -0700

    Sample data added and CLI args

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ from pprint import pprint
 import auth_flow
 import _url_object
 import webbrowser
-import sys
+import sys, getopt
 from Write import IOwrite
 
-OBJ_GOOGLE_NAME = input("Enter Sheets Name: ")
+def main(argv):
+    inputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"","ifile=")
+    except getopt.GetoptError:
+        print ('main.py -i <inputfile>')
+        sys.exit(2)
+    inputfile = args[0]
+    print('Test Data input: ', inputfile)
 
-OBJ_GOOGLE_AUTH = auth_flow.AUTHENTICATE_FLOW()
-OBJ_GOOGLE_SHEETS = OBJ_GOOGLE_AUTH.create(OBJ_GOOGLE_NAME)
-REPORT_URL = _url_object.default_url + OBJ_GOOGLE_SHEETS.id
+    OBJ_GOOGLE_NAME = input("Enter Output Sheets Name: ")
 
-# Active Sheet
-s = IOwrite.openSheet(client = OBJ_GOOGLE_AUTH, key = OBJ_GOOGLE_SHEETS.id)
-# Active Worksheet
-activeWS = IOwrite.openWorkSheet(s)
-# Write Sample Data
-activeWS.update_cell(1,1,"Test Line")
+    OBJ_GOOGLE_AUTH = auth_flow.AUTHENTICATE_FLOW()
+    OBJ_GOOGLE_SHEETS = OBJ_GOOGLE_AUTH.create(OBJ_GOOGLE_NAME)
+    REPORT_URL = _url_object.default_url + OBJ_GOOGLE_SHEETS.id
 
-if __name__ == '__main__':
+    # Active Sheet
+    s = IOwrite.openSheet(client = OBJ_GOOGLE_AUTH, key = OBJ_GOOGLE_SHEETS.id)
+    # Active Worksheet
+    activeWS = IOwrite.openWorkSheet(s)
+    # Write Sample Data
+    activeWS.update_cell(1,1,"Test Line")
+
     print("Report output: %s"%REPORT_URL)
     webbrowser.open_new(REPORT_URL)
 
+if __name__ == '__main__':
+    main(sys.argv[1:])
